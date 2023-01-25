b31eb2fb0907bfa9dbefb734acdc649062313484
Eric Ihle <zekeotronic@gmail.com>
2023-01-24 21:40:39 -0500

    adds Woo class and constructor

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+class Woo {
+  constructor(url, key, secret, options = {
+    wpAPIPrefix: '',
+    version: 'wc/v3',
+    encoding: 'utf-8',
+    queryStringAuth: false,
+    port: '',
+    timeout: 0
+  }) {
+    this.client = new WooCommerceRestApi({
+      url: url,
+      consumerKey: key,
+      consumerSecret: secret,
+      version: options.version,
+      wpAPIPrefix: options.wpAPIPrefix,
+      encoding: options.encoding,
+      queryStringAuth: options.queryStringAuth,
+      port: options.port,
+      timeout: options.timeout
+    });
+  }
 }
+
+
+module.exports = Woo;
